3ffaeac5cedda3a3d007504caaceaf5981eb38bf
Marcos Walker <marcoswalker@bol.com.br>
2025-03-29 20:44:52 -0300

    fv12

---
+++ readme.md
@@ -0,0 +1,83 @@
+# Token Magic FX -> Efeitos Mágicos Tagmar
+![](page/token-magic.gif)
+## Módulo para sistema Tagmar RPG no Foundry Vtt
+### Esse módulo depende do [Token Magic FX](https://foundryvtt.com/packages/tokenmagic) e do sistema [Tagmar RPG](https://foundryvtt.com/packages/tagmar) para funcionar.
+Adiciona nos itens de Tecnicas de Combate e Magia, a possíbilidade de adicionar filtros FX que serão executados no token sempre que os mesmos são rolados. Para remover os filtros de efeitos basta usar a macro "00 - A - Delete filters on Selected" que vem no compêndio do módulo **Token Magic FX**, ou na ficha de Personagem foi adicionado um ícone ao lado das caixas de busca, nas abas de Combate e Magia.
+
+#### Instalação:
+Copiar o link (https://raw.githubusercontent.com/marcoswalker/tagmar-token-fx/master/module.json) e colar na caixa manifest ao instalar módulo no Foundry Vtt. Lembrando que as dependências devem ser instaladas. Depois é só ativar os dois módulos dentro do mundo.
+
+#### Agradecimento (Thanks)
+Muito obrigado ao **SecretFire** por desenvolver o **Token Magic FX**.
+Many thanks to **SecretFire** for developing the **Token Magic FX**.
+
+#### Criar novos Pressets
+Para criar novos pressets bastar preencher a variável "params" e trocar o nome do presset dentro da variável "presetDef" em "name". Depois basta executar apenas uma vez como um macro.
+~~~javascript
+let params = [];
+ 
+ 
+let presetDef =
+{
+    name: "Nome do Presset",
+    library: "tmfx-template"
+};
+TokenMagic.addPreset(presetDef, params);
+~~~
+
+
+#### Deletar Pressets existentes
+Para deletar um presset basta mudar o "Nome do Presset" e executar apenas uma vez como uma macro.
+~~~javascript
+TokenMagic.deletePreset({name:"Nome do Presset",library:"tmfx-template"});
+~~~
+
+#### Presset para Metamorphose
+Escolha entre os nove tipos e preencha o número referente em "transitionType", mude o endereço da imagem em "targetImagePath", escolha um ID (nome único, sem acento, sem espaço) em "polymorphFilterId" e mude "name" dentro de "presetDef". Basta executar apenas uma vez a macro que fica cadastrado nos Pressets.
+~~~javascript
+// There is 9 types of metamorphose
+// 1 - Simple transition
+// 2 - Dreamy
+// 3 - Twist
+// 4 - Water drop
+// 5 - TV Noise
+// 6 - Morphing
+// 7 - Take off/Put on you disguise!
+// 8 - Wind
+// 9 - Hologram
+ 
+let transitionType = 6; // Escolhe o tipo de transição aqui, referente aos números de cima
+ 
+let targetImagePath = "icons/svg/mystery-man.svg"; // Troca o endereço da imagem aqui.
+ 
+let polymorphFilterId = "transformaMystery"; // Escolhe um nome ID( único, sem espaços, sem acentos )
+ 
+let params =
+[{
+    filterType: "polymorph",
+    filterId: polymorphFilterId,
+    type: transitionType,
+    padding: 70,
+    magnify: 1,
+    imagePath: targetImagePath,
+    animated:
+    {
+        progress:
+        {
+            active: true,
+            animType: "halfCosOscillation",
+            val1: 0,
+            val2: 100,
+            loops: 1,
+            loopDuration: 1000
+        }
+    }
+}];
+ 
+let presetDef =
+{
+    name: "Transforma Mystery Man", // Troca o nome aqui ( nome apresentável )
+    library: "tmfx-template"
+};
+TokenMagic.addPreset(presetDef, params);
+~~~
---
+++ scripts/main.js
@@ -0,0 +1,102 @@
+Hooks.on('tagmar_itemRoll', async function (rollItem, user) {
+    if (game.user !== user) return;
+    const magicFlags = rollItem.getFlag('tagmar-token-fx', 'magicFx');
+    if (magicFlags) {
+        for (let magic of magicFlags) {
+            let pstParams = {
+                name: magic,
+                library: "tmfx-template"
+            };
+            if (TokenMagic.getPreset(pstParams)) await TokenMagic.addUpdateFiltersOnSelected(TokenMagic.getPreset(pstParams), false);
+        }
+    }
+});
+
+Hooks.on("renderActorSheet", function (sheet, html, character) {
+    if (character.actor.type !== "Personagem") return;
+    $(html.find(".searchMagia")).css('width','90%');
+    $("<a class='removeFX' style='margin-left:20px;' title='Remover Efeitos Mágicos'><i class='fas fa-hand-sparkles'></i></a>").insertAfter(html.find('.searchMagia'));
+    $(html.find(".searchCombate")).css('width','90%');
+    $("<a class='removeFX' style='margin-left:20px;' title='Remover Efeitos Mágicos'><i class='fas fa-hand-sparkles'></i></a>").insertAfter(html.find('.searchCombate'));
+    html.find('.removeFX').click(function (event) {
+        TokenMagic.deleteFiltersOnSelected();
+    });
+});
+
+Hooks.on('ready', function () {
+    if (!(game.system.id === "tagmar_rpg" || game.system.id === "tagmar")) return ui.notifications.error("Esse módulo só funciona com o sistema Tagmar, não insista.");
+});
+
+Hooks.on("renderItemSheet", function (itemSheet, html, css) {
+    if (!(game.system.id === "tagmar_rpg" || game.system.id === "tagmar")) return ui.notifications.error("O módulo Token Magix Fx -> Efeitos Mágicos Tagmar, só funciona com o sistema Tagmar.");
+    if (!game.modules.get('tokenmagic')) return;
+    let tmfxTemplatePresets = TokenMagic.getPresets("tmfx-template");
+    if (itemSheet.item.type === "Magia" && itemSheet.item.actor !== null) {
+        //html.find('nav').append('<label class="mediaeval" style="border:black double 1px; text-align: center;"><a class="item" data-tab="tokenfx" title="Token Magic FX">Efeitos Mágicos</a></label>');
+        $.get("/modules/tagmar-token-fx/templates/magia.hbs", function (data) {
+            html.find('.sheet-primary').first().append(data);
+            for (let efect of tmfxTemplatePresets) {
+                html.find('.magia_fx').append("<option class='mediaeval' value='"+ efect.name +"'>"+ efect.name + "</option>");
+            }
+            html.find(".addMagicFx").click(function (event) {
+                const magicEfect = html.find(".magia_fx").val();
+                let ImagicFlags = itemSheet.item.getFlag('tagmar-token-fx', 'magicFx');
+                if (typeof ImagicFlags === "undefined" || typeof ImagicFlags === "string") {
+                    ImagicFlags = [magicEfect];
+                } else {
+                    ImagicFlags.push(magicEfect);
+                }
+                itemSheet.item.setFlag('tagmar-token-fx', 'magicFx', ImagicFlags);
+                event.preventDefault();
+            });
+            const magicFlags = itemSheet.item.getFlag('tagmar-token-fx', 'magicFx');
+            if (magicFlags) {
+                for (let magic of magicFlags) {
+                    html.find('.table_fx').append("<tr style='text-align:center;'><td>" + magic + "</td><td><a class='removeMagic' data-item-id='"+magic+"'><i class='fas fa-hand-sparkles'></i></a></td></tr>");
+                }
+            }
+            html.find('.removeMagic').click(function (event) {
+                let magicF = itemSheet.item.getFlag('tagmar-token-fx', 'magicFx');
+                let indexM = magicF.indexOf($(event.currentTarget).data("itemId"));
+                if (indexM !== -1) {
+                    magicF.splice(indexM, 1);
+                    itemSheet.item.setFlag('tagmar-token-fx', 'magicFx', magicF);
+                }
+                event.preventDefault();
+            });
+        });
+    } else if (itemSheet.item.type === "Tecnica_Combate" && itemSheet.item.actor !== null) {
+        $.get('/modules/tagmar-token-fx/templates/tecnica.hbs', function (data) {
+            
+            html.find('.sheet-primary').append(data);
+            //html.find('.sheet-header .container').first().append(data);
+            for (let efect of tmfxTemplatePresets) {
+                html.find('.magia_fx').append("<option class='mediaeval' value='"+ efect.name +"'>"+ efect.name + "</option>");
+            }
+            html.find(".addMagicFx").click(function () {
+                const magicEfect = html.find(".magia_fx").val();
+                let ImagicFlags = itemSheet.item.getFlag('tagmar-token-fx', 'magicFx');
+                if (typeof ImagicFlags === "undefined" || typeof ImagicFlags === "string") {
+                    ImagicFlags = [magicEfect];
+                } else {
+                    ImagicFlags.push(magicEfect);
+                }
+                itemSheet.item.setFlag('tagmar-token-fx', 'magicFx', ImagicFlags);
+            });
+            const magicFlags = itemSheet.item.getFlag('tagmar-token-fx', 'magicFx');
+            if (magicFlags) {
+                for (let magic of magicFlags) {
+                    html.find('.table_fx').append("<tr style='text-align:center;'><td>" + magic + "</td><td><a class='removeMagic' data-item-id='"+magic+"'><i class='fas fa-hand-sparkles'></i></a></td></tr>");
+                }
+            }
+            html.find('.removeMagic').click(function (event) {
+                let magicF = itemSheet.item.getFlag('tagmar-token-fx', 'magicFx');
+                let indexM = magicF.indexOf($(event.currentTarget).data("itemId"));
+                if (indexM !== -1) {
+                    magicF.splice(indexM, 1);
+                    itemSheet.item.setFlag('tagmar-token-fx', 'magicFx', magicF);
+                }
+            });
+        });
+    }
+});
